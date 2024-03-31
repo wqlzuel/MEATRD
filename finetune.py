cf81273c6b29bf2e01b9169c76b21218c18c0f74
wqlzuel <132863355+wqlzuel@users.noreply.github.com>
2024-03-31 13:16:20 +0800

    first commit

--- finetune.py
+++ finetune.py
@@ -5,11 +5,9 @@ import torch.nn as nn
 import torch.optim as optim
 from tqdm import tqdm
 from typing import Optional, List
-
 from .model import Generator, SVDDEncoder
 from .model import SSIMLoss, SVDDLoss, SCELoss
-from .utils import seed_everything
-
+from utils import seed_everything
 
 class SNNet:
     def __init__(self, epochs: List[int] = [10, 5], batch_size: int = 128,
