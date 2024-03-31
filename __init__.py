92c9e7aca9d524f7f8f3eea2ee78839b1e4f93bd
wqlzuel <132863355+wqlzuel@users.noreply.github.com>
2024-03-31 13:47:30 +0800

    first commit

--- __init__.py
+++ __init__.py
@@ -1,4 +1,3 @@
-from .main import SNNet
+from .finetune import SNNet
 from .pretrain import pretrain
-from .read import read, read_cross, read_multi, read_multi_cross, Build_graph, Build_multi_graph
-from ._utils import evaluate
+from .utils import evaluate
